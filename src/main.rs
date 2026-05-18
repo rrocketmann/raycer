34fe0a58ed36bc6dbebd2181d3f424b562b88a49
rrocketmann <martinmalyshau@gmail.com>
2026-05-18 16:21:42 -0700

    Add web deployment config (trunk + WASM)

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ mod track;
 mod ui;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "raycer".into(),
-                ..default()
-            }),
+    let window_plugin = WindowPlugin {
+        primary_window: Some(Window {
+            title: "raycer".into(),
+            canvas: Some("#raycer".into()),
             ..default()
-        }))
+        }),
+        ..default()
+    };
+
+    App::new()
+        .add_plugins(DefaultPlugins.set(window_plugin))
         .add_plugins(bevy_egui::EguiPlugin::default())
         .add_plugins((car::CarPlugin, track::TrackPlugin, ui::UiPlugin))
         .run();
